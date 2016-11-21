97e9619b22a3fbef5273da1dbf70f2d8df911bed
Alpha <alphagma@gmail.com>
2016-11-20 20:37:58 -0500

    Added coverage report to tests

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var mocha = require('gulp-mocha');
 var gutil = require('gulp-util');
+var istanbul = require('gulp-istanbul');
 
 const sourcesGlob = ['**/*.js','!node_modules/**'];
 const testsGlob = ['tests/**/*.js'];
@@ -17,9 +18,16 @@ gulp.task('lint', () =>
         .pipe(eslint.failAfterError())
 );
 
-gulp.task('test', () =>
+gulp.task('instrument-code', () =>
+    gulp.src(sourcesGlob)
+        .pipe(istanbul())
+        .pipe(istanbul.hookRequire())
+);
+
+gulp.task('test', ['instrument-code'], () =>
     gulp.src(testsGlob)
         .pipe(mocha({reporter: 'nyan'}))
+        .pipe(istanbul.writeReports())
 );
 
 gulp.task('watch-test', () =>
