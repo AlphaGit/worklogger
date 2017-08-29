6488c463de56a12a5c8193b5bf9865ed18d93f12
Alpha <alphagma@gmail.com>
2017-08-28 20:38:41 -0400

    Mapper tests

---
+++ tests/inputs/GoogleCalendarInput/GoogleCalendarToModelMapper.js
@@ -0,0 +1,104 @@
+const assert = require('assert');
+const GoogleCalendarToModelMapper = require('inputs/GoogleCalendarInput/GoogleCalendarToModelMapper');
+const Worklog = require('model/Worklog');
+
+describe('GoogleCalendarToModelMapper', () => {
+    describe('#constructor', () => {
+        it('takes in a configuration parameter', () => {
+            const mapper = new GoogleCalendarToModelMapper({ minimumLoggableTimeSlotInMinutes: 17 });
+            assert.equal(17, mapper.minimumLoggableTimeSlotInMinutes);
+        });
+    });
+
+    describe('#map', () => {
+        describe('maps to a Worklog with the correct information', () => {
+            const mapper = getMapper();
+            const apiResponse = getTestApiResponse({
+                client: 'My client',
+                project: 'My project',
+                eventName: 'My event name',
+                startDateTime: '2017-08-28T20:30:00-05:00',
+                endDateTime: '2017-08-28T21:00:00-05:00'
+            });
+
+            const result = mapper.map([apiResponse]);
+
+            assert.equal(1, result.length);
+            assert(result[0] instanceof Worklog);
+            assert.equal('My client', result[0].client);
+            assert.equal('My project', result[0].project);
+            assert.equal('My event name', result[0].name);
+            assert.equal(Date.parse('2017-08-28T20:30:00-05:00'), result[0].startDateTime);
+            assert.equal(Date.parse('2017-08-28T21:00:00-05:00'), result[0].endDateTime);
+            assert.equal(30, result[0].duration);
+        });
+
+        describe('flattens arrays of arrays of events to a single array', () => {
+            const mapper = getMapper();
+            const apiResponse = getTestApiResponse({ eventCount: 2 });
+            const secondApiResponse = getTestApiResponse({ eventCount: 3 });
+
+            const result = mapper.map([apiResponse, secondApiResponse]);
+
+            assert.equal(5, result.length);
+        });
+
+        describe('restricts events to a configurable minimum loggable time slot', () => {
+            const mapper = getMapper({ minimumLoggableTimeSlotInMinutes: 60 });
+            const apiResponse = getTestApiResponse({
+                client: 'My client',
+                project: 'My project',
+                eventName: 'My event name',
+                startDateTime: '2017-08-28T20:30:00-05:00',
+                endDateTime: '2017-08-28T21:00:00-05:00'
+            });
+
+            const result = mapper.map([apiResponse]);
+
+            assert.equal(1, result.length);
+            assert.equal(60, result[0].duration);
+        });
+    });
+});
+
+// Stubs for the instantiation of the test subject
+
+function getMapper({
+    minimumLoggableTimeSlotInMinutes = 30
+} = {}) {
+    const configuration = {
+        minimumLoggableTimeSlotInMinutes: minimumLoggableTimeSlotInMinutes
+    };
+    return new GoogleCalendarToModelMapper(configuration);
+}
+
+function getTestApiResponse({
+    client = 'Test client',
+    project = 'Test project',
+    eventName = 'Test event',
+    startDateTime = new Date().toISOString(),
+    endDateTime = new Date(Date.now() + (1000 * 60 * 30)).toISOString(), // 30 mins later
+    eventCount = 1,
+} = {}) {
+    let apiResponse = {
+        calendarConfig: {
+            client: client,
+            project: project,
+        },
+        events: []
+    };
+
+    for (let i = 1; i <= eventCount; i++) {
+        apiResponse.events.push({
+            summary: eventName,
+            start: {
+                dateTime: startDateTime
+            },
+            end: {
+                dateTime: endDateTime
+            }
+        });
+    }
+
+    return apiResponse;
+}
