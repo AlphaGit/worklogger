99da57f5bff9d47c09d721a62d781366f5ea8816
Alpha <alphagma@gmail.com>
2020-12-22 02:17:18 -0500

    Formatters

--- app/formatters/NoFormatFormatter.js
+++
@@ -1,7 +0,0 @@
-const FormatterBase = require('./FormatterBase');
-
-module.exports = class NoFormatFormatter extends FormatterBase {
-    format(worklogSet) {
-        return worklogSet;
-    }
-};
