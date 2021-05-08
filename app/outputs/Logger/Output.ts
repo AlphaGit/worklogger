4c5dbc536dce00672d6ec0ed9a6b91932446d9f6
Alpha <alphagma@gmail.com>
2021-05-08 14:44:51 +0000

    Fixed lint

--- app/outputs/Logger/Output.ts
+++ app/outputs/Logger/Output.ts
@@ -1,17 +1,18 @@
 import { OutputBase } from '../OutputBase';
-import { LoggerFactory } from '../../services/LoggerFactory';
 import { FormatterBase } from '../../formatters/FormatterBase';
 import { AppConfiguration } from '../../models/AppConfiguration';
 import { WorklogSet } from '../../models/WorklogSet';
 import { IOutputConfiguration } from '../IOutputConfiguration';
 
+import { getLogger, Logger } from 'log4js';
+
 export class LoggerOutput extends OutputBase {
-    _logger: any;
+    _logger: Logger;
 
     constructor(formatter: FormatterBase, outputConfiguration: IOutputConfiguration, appConfiguration: AppConfiguration) {
         super(formatter, outputConfiguration, appConfiguration);
 
-        this._logger = LoggerFactory.getLogger('Logger/Output');
+        this._logger = getLogger();
     }
 
     outputWorklogSet(worklogSet: WorklogSet): void {
