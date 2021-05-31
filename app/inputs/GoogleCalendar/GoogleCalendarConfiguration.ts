5684e7387af529c8c02047a01e5ddd41c2816dc5
Alpha <alphagma@gmail.com>
2021-05-31 08:21:38 -0400

    GoogleCalendarInput (WIP)

---
+++ app/inputs/GoogleCalendar/GoogleCalendarConfiguration.ts
@@ -0,0 +1,10 @@
+import { InputConfiguration } from "../../models/InputConfiguration";
+
+export class GoogleCalendarConfiguration extends InputConfiguration {
+    calendars: GoogleCalendarCalendarConfiguration[];
+}
+
+export class GoogleCalendarCalendarConfiguration  {
+    id: string;
+    includeTags: string[];
+}
