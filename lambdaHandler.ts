9d7816a0c7a4d36e271997d11c79e37fd47b62bd
Alpha <alphagma@gmail.com>
2025-06-09 20:28:13 -0400

    Handle missing query parameters

--- lambdaHandler.ts
+++ lambdaHandler.ts
@@ -15,16 +15,27 @@ export const logTimesheets = async (event: APIGatewayProxyEvent): Promise<APIGat
 };
 
 export const storeToken = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    if (event.queryStringParameters.error) {
+    const { code, error } = event.queryStringParameters ?? {};
+
+    if (error) {
         return {
             statusCode: 400,
             body: JSON.stringify({
-                error: event.queryStringParameters.error
+                error
             })
         };
     }
 
-    await storeGoogleTokenFromCode(event.queryStringParameters.code);
+    if (!code) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                error: 'Missing code or error.'
+            })
+        };
+    }
+
+    await storeGoogleTokenFromCode(code);
 
     return {
         statusCode: 200,
